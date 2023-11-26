ad89de0876d0154974eb07f2ab0e25a04e399dab
Anastasiya <puchinina.anastasia58@gmail.com>
2023-11-27 02:22:42 +0700

    Create class Item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,24 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+item1 = Item('Микроволновая печь', 3000, 50)
+item2 = Item('Телевизор', 50000, 10)
+Item.pay_rate = 0.5
+
+def test_item():
+    assert item1.name == 'Микроволновая печь'
+    assert item2.name == 'Телевизор'
+    assert item1.price == 3000
+    assert item2.price == 50000
+    assert item1.quantity == 50
+    assert item2.quantity == 10
+
+
+def test_calculate_total_price():
+    assert item1.calculate_total_price() == 150000
+    assert item2.calculate_total_price() == 500000
+
+def test_apply_discount():
+    item2.apply_discount()
+    assert item1.price == 3000
+    assert item2.price == 25000
+
