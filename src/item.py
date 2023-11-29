e4de1f9615c20eef47e0fcd9daec550225c799c0
Anastasiya <puchinina.anastasia58@gmail.com>
2023-11-29 15:52:23 +0700

    Update Item, tests

--- src/item.py
+++ src/item.py
@@ -1,11 +1,13 @@
-import gc
+import csv
+import typing
+
 
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
-    all = gc.get_objects()
+    all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -15,7 +17,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
@@ -32,6 +34,38 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, file_name: typing.Any) -> None:
+        """
+        Создает экземпляры класса из файла csv.
+        """
+        with open(file_name, "r") as file:
+            dict_reader = csv.DictReader(file, delimiter=",")
+            for row in dict_reader:
+                name = row["name"]
+                price = Item.string_to_number(row["price"])
+                quantity = Item.string_to_number(row["quantity"])
+                Item.all.append(cls(name, price, quantity))
+
+                
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value[:10]
+
+    @staticmethod
+    def string_to_number(string: str) -> int:
+        """
+        Преобразование строки в число.
+        """
+        if string.isdigit():
+            return int(string)
+        return int(float(string))
 
 
 
--- tests/test_item.py
+++ tests/test_item.py
@@ -4,6 +4,7 @@ item1 = Item('Микроволновая печь', 3000, 50)
 item2 = Item('Телевизор', 50000, 10)
 Item.pay_rate = 0.5
 
+
 def test_item():
     assert item1.name == 'Микроволновая печь'
     assert item2.name == 'Телевизор'
@@ -17,8 +18,33 @@ def test_calculate_total_price():
     assert item1.calculate_total_price() == 150000
     assert item2.calculate_total_price() == 500000
 
+
 def test_apply_discount():
     item2.apply_discount()
     assert item1.price == 3000
     assert item2.price == 25000
 
+
+def test_string_to_number():
+    assert Item.string_to_number('10.3') == 10
+    assert Item.string_to_number('10.0') == 10
+
+    assert Item.string_to_number('10') == 10
+
+
+def test_name():
+    item1.name = 'Микроволновая печь'
+    item2.name = 'Пылесос'
+    assert item1.name == 'Микроволно'
+    assert item2.name == 'Пылесос'
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv("../src/items.csv")
+    assert len(Item.all) == 5
+    item_one = Item.all[2]
+    item_two = Item.all[3]
+    assert item_one.name == "Кабель"
+    assert item_one.price == 10
+    assert item_two.name == "Мышка"
+    assert item_two.quantity == 5
